3f854162c51839ee9a0ff805af4fb4a561be8b14
Denisoed <denisod93@gmail.com>
2018-10-21 11:47:08 +0600

    added command for displayed generated standup in telegram

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import logging
+from handlers.parser_jira import generateStandup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
 
@@ -9,10 +10,17 @@ logging.basicConfig(level=logging.DEBUG, \
 
 # Start function
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.send_message(chat_id=update.message.chat_id, text="Бот для генерирования стендапов")
+
+def standup(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Обработка данных...")
+    bot.send_message(chat_id=update.message.chat_id, text="%s" % generateStandup())
 
 # handler send command(=> /start <=)
 _UPDATER.dispatcher.add_handler(CommandHandler('start', start))
+
+# handler send command(=> /standup <=)
+_UPDATER.dispatcher.add_handler(CommandHandler('standup', standup))
 
 _UPDATER.start_polling()
 _UPDATER.idle()
--- handlers/parser_jira.py
+++ handlers/parser_jira.py
@@ -12,8 +12,17 @@ def getWorklogs(issues):
 
 def getYesterday():
     now = datetime.datetime.now()
-    return "%d/%d/%d" % (now.year, now.month, now.day - 1)
+    return "%d/%d/%d" % (now.year, now.month, now.day - 2)
 
 def getYesterdayWorklogIssues():
     yesterday_issues_worklogs = jira.search_issues('worklogAuthor = currentUser() AND worklogDate = "%s"' % getYesterday())
     return getWorklogs(yesterday_issues_worklogs)
+
+def generateStandup():
+    issues_with_worklogs = getYesterdayWorklogIssues()
+    yesterday = ''
+    for issue in issues_with_worklogs:
+        yesterday += '- {0} https://nappyclub.atlassian.net/browse/{1}\n'.format(issue.fields.worklog.worklogs[0].comment, issue.key)
+
+    standup = 'Доброе утро!\n\n*Вчера*\n%s\n*Сегодня*\n%s\n\n*Проблемы*\n%s' % (yesterday, '- Чинить баги', '- Нет проблем!')
+    return standup
